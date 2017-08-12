ba913ab1b414dbeb04cd7d305a560f79e190c2fd
James Slattery <james@slattery.tech>
2017-08-12 00:13:46 -0500

    Login (basic) working.

---
+++ client/func/authenticate.js
@@ -0,0 +1,35 @@
+// By: James Slattery
+
+
+// Login Logic
+Template.base.events({
+  'submit .login': function () {
+    let email = $('[name="email"]').val();
+    let password = $('[name="password"]').val();
+
+    if ( email.length() < 1 ) {
+      console.log("Email Required");
+    }
+    if ( password.length() < 1) {
+      console.log("Password Required");
+    }
+
+
+    Meteor.loginWithPassword(email, password);
+  }
+})
+
+
+
+// Register Logic
+Template.register.events({
+  'submit .register': function () {
+    event.preventDefault();
+    //let firstName = $('[name="firstName"]').val();
+    //let lastName = $('[name="lastName"]').val();
+    let email = $('[name="email"]').val();
+    let password = $('[name="password"]').val();
+    // let role (need to config)
+    Accounts.createUser({email: email, password : password});
+  }
+});
